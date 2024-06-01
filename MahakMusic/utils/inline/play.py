4f697b43f239e6d95a505016d433b493c46b8a08
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2024-06-01 14:19:07 +0530

    Update play.py

--- MahakMusic/utils/inline/play.py
+++ MahakMusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "Ⓐ▰▱▱▱▱▱▱▱▰"
+        bar = "ⓢ▰▱▱▱▱▱▱▱▰"
     elif 10 < umm < 20:
-        bar = "▰Ⓐ▱▱▱▱▱▱▱▰"
+        bar = "▰ⓢ▱▱▱▱▱▱▱▰"
     elif 20 <= umm < 30:
-        bar = "▰▱Ⓐ▱▱▱▱▱▱▰"
+        bar = "▰▱ⓢ▱▱▱▱▱▱▰"
     elif 30 <= umm < 40:
-        bar = "▰▱▱Ⓐ▱▱▱▱▱▰"
+        bar = "▰▱▱ⓢ▱▱▱▱▱▰"
     elif 40 <= umm < 50:
-        bar = "▰▱▱▱Ⓐ▱▱▱▱▰"
+        bar = "▰▱▱▱ⓢ▱▱▱▱▰"
     elif 50 <= umm < 60:
-        bar = "▰▱▱▱▱Ⓐ▱▱▱▰"
+        bar = "▰▱▱▱▱ⓢ▱▱▱▰"
     elif 60 <= umm < 70:
-        bar = "▰▱▱▱▱▱Ⓐ▱▱▰"
+        bar = "▰▱▱▱▱▱ⓢ▱▱▰"
     elif 70 <= umm < 80:
-        bar = "▰▱▱▱▱▱▱Ⓐ▱▰"
+        bar = "▰▱▱▱▱▱▱ⓢ▱▰"
     elif 80 <= umm < 95:
-        bar = "▰▱▱▱▱▱▱▱Ⓐ▰"
+        bar = "▰▱▱▱▱▱▱▱ⓢ▰"
     else:
-        bar = "▰▱▱▱▱▱▱▱▰Ⓐ"
+        bar = "▰▱▱▱▱▱▱▱▰ⓢ"
     buttons = [
         [
             InlineKeyboardButton(
@@ -68,11 +68,11 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text="ᴜᴘᴅᴀᴛᴇ",
-                url=f"https://t.me/roy_editx",
+                url=f"https://t.me/itsdroids",
             ),
             InlineKeyboardButton(
                 text="sᴜᴘᴘᴏʀᴛ",
-                url=f"https://t.me/the_friendz",
+                url=f"https://t.me/droidchats",
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -91,11 +91,11 @@ def stream_markup(_, chat_id):
         [
             InlineKeyboardButton(
                 text="ᴜᴘᴅᴀᴛᴇ",
-                url=f"https://t.me/roy_editx",
+                url=f"https://t.me/itsdroids",
             ),
             InlineKeyboardButton(
                 text="sᴜᴘᴘᴏʀᴛ",
-                url=f"https://t.me/the_friendz",
+                url=f"https://t.me/droidchats",
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
